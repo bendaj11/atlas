4dddc58e23c384cb1530e9c3bed5fcd2c8c07419
jbd <bendaj11@gmail.com>
2026-07-05 22:23:43 +0300

    release beta

--- packages/generators/src/cli/generator-versions.ts
+++ packages/generators/src/cli/generator-versions.ts
@@ -1,6 +1,6 @@
 import type { AtlasGeneratorOptions } from "./generator-types.js";
 
-export const ATLAS_PACKAGE_VERSION = "0.1.0";
+export const ATLAS_PACKAGE_VERSION = "0.2.0";
 
 export function atlasPackageRange(): string {
   return `^${ATLAS_PACKAGE_VERSION}`;
